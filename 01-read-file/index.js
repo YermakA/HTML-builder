c6efd81949080d528f16045d6c5ccce7a2b4f80a
YermakA <ermak.080@mail.ru>
2024-01-19 16:32:54 +0800

    feat: 01 read file - solved

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const process = require('process');
+
 const stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), {
   encoding: 'utf-8',
 });
@@ -8,6 +8,6 @@ const stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), {
 if (stream) {
   stream.on('readable', function () {
     let chunk = stream.read();
-    if (chunk !== null) process.stdout.write(chunk.toString());
+    if (chunk !== null) process.stdout.write(chunk);
   });
 }
